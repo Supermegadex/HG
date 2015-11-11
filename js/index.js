7fa481477719f0371b3e5c06abc7932f665cc0ab
Daniel Noon <supercookies@outlook.com>
2015-11-11 15:27:58 -0600

    Added a play card (which doesn't work)

--- js/index.js
+++ js/index.js
@@ -1,11 +1,29 @@
-var people = new Array(0);
+var people = new Array( 0 );
 
 function nextChar() {
-  temp.name = document.querySelector("#name").value;
-  temp.str = document.querySelector("#strSlider").value;
-  temp.cha = document.querySelector("#chaSlider").value;
-  
-  people.push(temp);
-  window.location.hash = "tab1";
+  temp.name = document.querySelector( "#name" ).value;
+  temp.str = document.querySelector( "#strSlider" ).value;
+  temp.cha = document.querySelector( "#chaSlider" ).value;
+  temp.int = document.querySelector( "#intSlider" ).value;
+  temp.agi = document.querySelector( "#agiSlider" ).value;
+
+  people.push( temp );
   document.querySelector("#create").innerHTML = "<char-card></char-card>";
+  location.hash = "bdy";
+      document.querySelector("#addToast").text = 'Person "' + temp.name + '" added to roster.';
+      document.querySelector("#addToast").show();
+}
+
+function switchTab(i){
+  document.querySelector('#app').selected = Number(i);
+}
+function done() {
+  nextChar();
+  document.querySelector("#cont3").innerHTML = "<paper-button onclick='start()'>Begin</paper-button>";
+  switchTab(2);
+  document.querySelector("#create").innerHTML = "";
+}
+function start() {
+  document.querySelector("#cont1").innerHTML = "<play-card id='playCard' day='1' time='bloodbath'></play-card>";
+  switchTab(0);
 }

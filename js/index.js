05656423bb1c37a3a1cd3eb77026d0eec511d6c2
Daniel Noon <supercookies@outlook.com>
2015-11-17 12:33:53 -0600

    Added a header and scroll. Larger tabs, etc.

--- js/index.js
+++ js/index.js
@@ -8,22 +8,28 @@ function nextChar() {
   temp.agi = document.querySelector( "#agiSlider" ).value;
 
   people.push( temp );
-  document.querySelector("#create").innerHTML = "<char-card></char-card>";
+  document.querySelector( "#create" ).innerHTML = "<char-card></char-card>";
   location.hash = "bdy";
-      document.querySelector("#addToast").text = 'Person "' + temp.name + '" added to roster.';
-      document.querySelector("#addToast").show();
+  document.querySelector( "#addToast" ).text = 'Person "' + temp.name + '" added to roster.';
+  document.querySelector( "#addToast" ).show();
 }
 
-function switchTab(i){
-  document.querySelector('#app').selected = Number(i);
+function switchTab( i ) {
+  window.location.href = "javascript:document.querySelector('#app').selected = Number(" + i + ")";
 }
+
 function done() {
   nextChar();
-  document.querySelector("#cont3").innerHTML = "<paper-button onclick='start()'>Begin</paper-button>";
-  switchTab(2);
-  document.querySelector("#create").innerHTML = "";
+  var pn;
+  for ( var i in people ) {
+    
+  }
+  document.querySelector( "#cont3" ).innerHTML = "<paper-button onclick='start()'>Begin</paper-button>" + pn;
+  switchTab( 2 );
+  document.querySelector( "#create" ).innerHTML = "";
 }
+
 function start() {
-  document.querySelector("#cont1").innerHTML = "<play-card id='playCard' day='1' time='bloodbath'></play-card>";
-  switchTab(0);
+  document.querySelector( "#cont1" ).innerHTML = "<play-card id='playCard' day='1' time='bloodbath'></play-card>";
+  switchTab( 0 );
 }
